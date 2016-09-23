ce79fd21172e374900a516bbcf2a8d15701062ec
holmfridurge <holmfridurge@gmail.com>
2016-09-23 13:31:44 +0000

    initial commit, IsLeapYear ready

---
+++ Services/Utilities/DateTimeUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace CoursesAPI.Services.Utilities
+{
+	public class DateTimeUtils
+	{
+		public static bool IsLeapYear(int year)
+		{
+			// TODO: add your logic here!!!1!!!one!!!eleven
+			if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+			{
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
